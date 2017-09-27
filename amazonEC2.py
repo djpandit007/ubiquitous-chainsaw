d8d599e802ed21c5b97d889a1f235d8a778f532e
Dhananjay <dj.pandit007@gmail.com>
2017-09-27 16:08:38 +0000

    Better error logs. Process doesn't quit if an error is raised

--- amazonEC2.py
+++ amazonEC2.py
@@ -13,20 +13,24 @@ token = TheTvDbEpisode.authenticate()
 todaysDate = datetime.now().date()
 
 for series in myFavouries:
-    seriesName, seriesTime = TheTvDbEpisode.getSeriesNameTime(series)
-    seriesNameFormatted = nextEpisode.tvShowURL(seriesName)
-    latestEpisodes = nextEpisode.previousNext(seriesNameFormatted)
-    later = latestEpisodes["nextEpisode"]
-    nextAirDate = later["airDate"]
-    if nextAirDate:
-        nextAirDateFormatted = datetime.strptime(nextAirDate[0], '%a %b %d, %Y').date()
-        if nextAirDateFormatted == todaysDate:
-            episodeToday = True
-            sendMessage += "%s airs at %s in %s\n" % (str(seriesName), str(seriesTime), str(later["countDown"][0]))
+    try:
+        seriesName, seriesTime = TheTvDbEpisode.getSeriesNameTime(series)
+        seriesNameFormatted = nextEpisode.tvShowURL(seriesName)
+        latestEpisodes = nextEpisode.previousNext(seriesNameFormatted)
+        later = latestEpisodes["nextEpisode"]
+        nextAirDate = later["airDate"]
+        if nextAirDate:
+            nextAirDateFormatted = datetime.strptime(nextAirDate[0], '%a %b %d, %Y').date()
+            if nextAirDateFormatted == todaysDate:
+                episodeToday = True
+                sendMessage += "%s airs at %s in %s\n" % (str(seriesName), str(seriesTime), str(later["countDown"][0]))
+    except Exception as err:
+	print "Exception occured for %s: %s" % (seriesName, err)
+
+
 
 print "CRON job date: " + str(todaysDate)
 if episodeToday:
     TheTvDbEpisode.sendSMS(sendMessage)
 else:
     TheTvDbEpisode.sendSMS("No series airing today")
-
